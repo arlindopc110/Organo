4cbd1b1e21be979ed762fb17dc1aefdaf72886f3
Arlindo Custodio <108751670+arlindopc110@users.noreply.github.com>
2023-09-04 22:20:11 -0300

    Add files via upload
    First commit

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,16 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (<footer className="footer">
+        <section>
+            <img src="/imagens/logo.png" alt="" />
+        </section>
+        <section>
+            <p>
+                Desenvolvido por Arlindo Custódio.
+            </p>
+        </section>
+    </footer>)
+}
+
+export default Rodape
